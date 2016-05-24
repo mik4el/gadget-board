6b088d70c5ff0edb9231d8449c6dd85ca9babb95
Mikael Andersson <mikael.andersson@volumental.com>
2016-05-24 10:28:50 +0200

    installed rest frame work and made auth app work with admin

--- web/authentication/models.py
+++ web/authentication/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.contrib.auth.models import BaseUserManager
 
@@ -24,17 +24,19 @@ class AccountManager(BaseUserManager):
         account = self.create_user(username, password, **kwargs)
 
         account.is_admin = True
+
         account.save()
 
         return account
 
 
-class Account(AbstractBaseUser):
+class Account(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=40, unique=True)
     email = models.EmailField()  # users can share email
 
     is_gadget = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -52,3 +54,9 @@ class Account(AbstractBaseUser):
 
     def get_short_name(self):
         return self.username
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
--- web/gadget_board_backend/urls.py
+++ web/gadget_board_backend/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from gadget_board_backend.views import IndexView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^.*$', IndexView.as_view(), name='index'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #url('^.*$', IndexView.as_view(), name='index'),
 ]
