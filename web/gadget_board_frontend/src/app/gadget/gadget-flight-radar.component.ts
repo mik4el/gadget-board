d222b454780ba919bc867b296e99aec1ac1e11f5
Mikael Andersson <mikael.andersson@volumental.com>
2016-10-22 13:42:35 +0200

    added distance to flight radar

--- web/gadget_board_frontend/src/app/gadget/gadget-flight-radar.component.ts
+++ web/gadget_board_frontend/src/app/gadget/gadget-flight-radar.component.ts
@@ -8,4 +8,18 @@ import { GadgetData } from './gadget-data';
 
 export class GadgetFlightRadarComponent {
     @Input() gadgetDatum: GadgetData;
+
+    distanceFromFlightRadar(lon1, lat1) {
+      var lat2 = 59.3147788;  // Flight radar position
+      var lon2 = 17.9863169;
+      var R = 6371; // Earth radius
+      var dLat = (lat2 - lat1) * Math.PI / 180;  // deg2rad below
+      var dLon = (lon2 - lon1) * Math.PI / 180;
+      var a =
+         0.5 - Math.cos(dLat)/2 +
+         Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+         (1 - Math.cos(dLon))/2;
+      return R * 2 * Math.asin(Math.sqrt(a));
+    }
+
 }
